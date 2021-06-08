15fa8a97e4832d3a5cb2d52933977f711c0e7caf
timothyAgevi <timothyagevi@gmail.com>
2021-06-08 23:08:38 +0300

    check if parenthesis are balanced

--- checkBalanced.js
+++ checkBalanced.js
@@ -22,3 +22,32 @@
 //  determines whether each sequence of brackets is balanced.
 //   If a string is balanced,
 //  return YES. Otherwise, return NO.
+
+const checkBalancedParantheses = (exp) => {
+    const stack = []
+
+    for (let i = 0; i < exp.length; i++) {
+        const element = exp.charAt(i);
+        const peek = stack[stack.length - 1];
+        const length = stack.length
+
+        if(element === ')' && peek === '('  && length !== 0){
+            stack.pop();
+
+        } else if(element === '}' && peek === '{'  && length !== 0){
+            stack.pop();
+
+        } else if(element === ']' && peek === '['  && length !== 0){
+            stack.pop();
+
+        } else {
+            stack.push(element);
+        }
+
+    }
+
+    console.log('Content of array are', stack);
+    if (stack.length === 0)  return true; return false;
+}
+
+checkBalancedParantheses('{[]()}')
