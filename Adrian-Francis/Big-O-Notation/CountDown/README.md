16dcd7e7c6a16f3fbd47fc98e7f978c22af220b3
Psycadelik <francisadrian9@hotmail.com>
2021-06-08 22:20:56 +0300

    add countdown solution

---
+++ Adrian-Francis/Big-O-Notation/CountDown/README.md
@@ -0,0 +1,10 @@
+Given an integer n as input countdown(n) prints the numbers n to 1 in descending order then prints "Blast off!"
+
+```
+def countdown(n):
+    while n > 0:
+        print(n)
+        n -= 1
+    print("Blast off!")
+
+```
---
+++ Adrian-Francis/Big-O-Notation/CountDown/count_down.py
@@ -0,0 +1,12 @@
+# Big-O Analysis
+# Time complexity: O(n)
+# Space Complexity: O(n)
+
+def countdown(n):
+    while n > 0: # n times
+        print(n) # 1 operation
+        n -= 1 # n times
+    print("Blast off!") # 1 operation
+# n * n + 2
+# O(n)**2
+# O(n)
