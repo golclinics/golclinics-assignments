46bab33b9c65e6a2996bee3658ad007ee89492a1
Psycadelik <francisadrian9@hotmail.com>
2021-06-08 22:55:42 +0300

    :update: update count_down solution

--- Adrian-Francis/Big-O-Notation/CountDown/count_down.py
+++ Adrian-Francis/Big-O-Notation/CountDown/count_down.py
@@ -1,12 +1,15 @@
 # Big-O Analysis
-# Time complexity: O(n)
-# Space Complexity: O(n)
+# Time complexity: O(2n)
+# Space Complexity: O(n^2)
 
 def countdown(n):
     while n > 0: # n times
         print(n) # 1 operation
         n -= 1 # n times
     print("Blast off!") # 1 operation
+# n * 1 + n * 1 + 1
+# n + n +1
+# 2n + 1
 # n * n + 2
 # O(n)**2
 # O(n)
