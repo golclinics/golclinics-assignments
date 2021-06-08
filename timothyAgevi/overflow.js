25b8348ed834c9dcd6824eb75dae376e103c1737
timothyAgevi <timothyagevi@gmail.com>
2021-06-08 22:53:54 +0300

    view steps

--- timothyAgevi/overflow.js
+++ timothyAgevi/overflow.js
@@ -1,3 +1,10 @@
 // As follow-up to the stack operations discussed in lesson 1, 
 // implement the overflow logic (extension of an 
 //     array when pushing an element to a full stack)
+
+//psedocode
+// double the stackLength
+// create a new Array of double the size
+// spread the old array to the new array
+// change the pointer of the stacks array to point to the new array
+// push the element into the new array
