d8a31f12ba915b6d5dadbdc2feb53fb4a7a20664
timothyAgevi <timothyagevi@gmail.com>
2021-06-22 22:39:34 +0300

    solve

---
+++ timothyAgevi/searching/assignment1.js
@@ -0,0 +1,25 @@
+/* 
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+You can return the answer in any order.
+Example 1:
+Input: nums = [2,7,11,15], target = 9 Output: [0,1] Output: Because nums[0] + nums[1] == 9, we return [0, 1].
+Example 2:
+Input: nums = [3,2,4], target = 6 Output: [1,2]
+Example 3:
+Input: nums = [3,3], target = 6 Output: [0,1] */
+
+const addUptoTarget = (arr, target) =>{
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = i+1; j < arr.length; j++) {
+            if(arr[i] + arr[j] === target) return [i,j];
+
+        }
+
+    }
+
+    return false;
+}
+
+console.log(addUptoTarget([1,2,5], 3)); // [0,1]
+console.log(addUptoTarget([2,7,11,15], 9)); // [0,1]
