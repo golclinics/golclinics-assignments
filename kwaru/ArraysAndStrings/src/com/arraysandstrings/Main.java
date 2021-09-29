1d1f2822dedfec909808b1f07855fa5af8e4174b
momondi <momondi547@gmail.com>
2021-09-29 16:04:01 +0300

    reverse integer array method implementation

--- kwaru/ArraysAndStrings/src/com/arraysandstrings/Main.java
+++ kwaru/ArraysAndStrings/src/com/arraysandstrings/Main.java
@@ -1,7 +1,5 @@
 package com.arraysandstrings;
 
-
-
 public class Main {
 
 
@@ -10,40 +8,67 @@ public class Main {
 
 
     public static void main(String[] args) {
-	// write your code here
-        int [] a = new int[]{1, 4, 5, 6};
 
-        reverseArray(a);
+        int [] A = new int[]{10, 5, 6, 9};
+
+       reverseArray(A);
 
     }
 
 
     /**
      *
-     * @param a, integer array
+     * @param A, integer array
      *   reverse the element of int array a
      */
-    static void reverseArray( int []a){
+    static void  reverseArray( int []A){
+    int temp;
+    int j=0;
+
+        if(A.length==0) // check if the array is  empty
+            return ;
+
+          int  reverseTimes=A.length/2; // i need to perform reverse approximately length divide by times
+                                         //to take of the last  sole element in case of odd number of elements
 
 
-        if(a.length==0) // check if the array is  empty
-            return;
+        for(int i=A.length-1; i>=0; i--) { // if not give print reversed
 
-        for(int i=a.length-1; i>=0; i--) { // if not give print reversed
+            temp= A[j];
+            A[j]=A[i];
+            A[i]=temp;
+            j++;
+            if(j==reverseTimes) // break when number reverses is reached
+                break;
 
-            if(i==a.length-1) {
-                System.out.print("[ " + a[i]);
-            }
-            else {
-                System.out.print("," + a[i]);
-            }
+
         }
-        System.out.print("]");
+        formatArray(A);// output array object
+return ;
+
+
+
+
 
     }
 
 
+    /**
+     *
+     * @param resultArray
+     * print array nicely eg [,9,6,5,10]
+     */
+    static void formatArray(int []resultArray){
+        int i=0;
+        System.out.print("[");
+        for(int k : resultArray){
 
+            if(i==0)System.out.print(+k);// the first element should not have , before
+            if(i>0)System.out.print(","+k);
+            i++;
+        }
+        System.out.print("]");
+    }
 
 }
 
