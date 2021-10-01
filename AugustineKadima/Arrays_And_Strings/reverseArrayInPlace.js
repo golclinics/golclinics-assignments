64aa6d73c9260e32cc8690fdb17988dc35ad6827
AugustineKadima <kadimasam@gmail.com>
2021-10-02 01:23:39 +0300

    Write a function to reverse an array of integers

---
+++ AugustineKadima/Arrays_And_Strings/reverseArrayInPlace.js
@@ -0,0 +1,18 @@
+
+
+const reverseArray = (A) => {
+    let n = A.length - 1;
+    let i;
+   
+    for(i = 0; i < n; i++){
+        let temp = A[n];
+        if(i < n){
+            A[n] = A[i];
+            A[i] = temp;
+            n--;
+        }
+    } 
+    console.log(A);
+
+}
+reverseArray([9, 6, 5, 10,7,4,3,4,5]);
