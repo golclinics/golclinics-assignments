59cd11a08aa67be4ad4398b0fa228f4255a4f49c
James Njuguna <jameskinyua590@gmail.com>
2021-10-21 22:47:09 +0300

    Format the parenthesesScore function

--- JayKay24/Stacks and Queues 1/balanced-parentheses.ts
+++ JayKay24/Stacks and Queues 1/balanced-parentheses.ts
@@ -4,21 +4,21 @@
  * @returns The score of the balanced parentheses
  */
 function parenthesesScore(s: string): number {
-	let score = 0;
-	const scoreStack: number[] = [];
-	const OPENER = '(';
+  let score = 0;
+  const scoreStack: number[] = [];
+  const OPENER = "(";
 
-	for (const char of s) {
-		if (char === OPENER) {
-			scoreStack.push(score);
-			score = 0;
-		} else {
+  for (const char of s) {
+    if (char === OPENER) {
+      scoreStack.push(score);
+      score = 0;
+    } else {
       // score is 1 when we find a matching closing parens, unless it was
       // already nested inside another pair of parens then we multiply it
       // by 2 and add that to the previous score tracked by the stack
       score = scoreStack.pop() + Math.max(score * 2, 1);
     }
-	}
+  }
 
-	return score;
+  return score;
 }
