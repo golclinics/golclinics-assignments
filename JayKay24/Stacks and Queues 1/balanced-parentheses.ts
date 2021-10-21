b6fe9c343658dcdce85ea4b3e71bd815dc213e5d
James Njuguna <jameskinyua590@gmail.com>
2021-10-21 22:45:09 +0300

    Implement solution for balanced parentheses score

---
+++ JayKay24/Stacks and Queues 1/balanced-parentheses.ts
@@ -0,0 +1,24 @@
+/**
+ * Return the score of the balanced parentheses
+ * @param s - Balanced parentheses
+ * @returns The score of the balanced parentheses
+ */
+function parenthesesScore(s: string): number {
+	let score = 0;
+	const scoreStack: number[] = [];
+	const OPENER = '(';
+
+	for (const char of s) {
+		if (char === OPENER) {
+			scoreStack.push(score);
+			score = 0;
+		} else {
+      // score is 1 when we find a matching closing parens, unless it was
+      // already nested inside another pair of parens then we multiply it
+      // by 2 and add that to the previous score tracked by the stack
+      score = scoreStack.pop() + Math.max(score * 2, 1);
+    }
+	}
+
+	return score;
+}
