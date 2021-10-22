6a19608145a96ef37dbce3e7598b5b33cddf84e6
devKiratu <dev.kiratu@gmail.com>
2021-10-22 12:55:13 +0300

    add solution to LeetCode Q20. Valid Parenthesis

---
+++ devKiratu/Stacks/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Stacks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var b0 = "()";
+            var b1 = "()[]{}";
+            var b2 = "(]";
+            var b3 = "([)]";
+            var b4 = "{[]}";
+            Console.WriteLine($"is {b0} valid? : {StackExercises.IsValid(b0)}");
+            Console.WriteLine($"is {b1} valid? : {StackExercises.IsValid(b1)}");
+            Console.WriteLine($"is {b2} valid? : {StackExercises.IsValid(b2)}");
+            Console.WriteLine($"is {b3} valid? : {StackExercises.IsValid(b3)}");
+            Console.WriteLine($"is {b4} valid? : {StackExercises.IsValid(b4)}");
+        }
+
+
+    }
+}
