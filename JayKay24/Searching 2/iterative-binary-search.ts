34bdc317ec14930890a10c5e37a821b7ce30bbb3
James Njuguna <jameskinyua590@gmail.com>
2021-11-02 22:21:25 +0300

    Implement iterative binary search

---
+++ JayKay24/Searching 2/iterative-binary-search.ts
@@ -0,0 +1,26 @@
+/**
+ * Returns idx of target, -1 if not found
+ * @param nums - an array of integers
+ * @param target - integer to search for
+ * @returns idx of target, -1 if not found
+ */
+function iterativeBinarySearch(nums: number[], target: number): number {
+  let leftIdx = 0,
+    rightIdx = nums.length - 1;
+
+  while (leftIdx <= rightIdx) {
+    const midIdx = Math.floor((leftIdx + rightIdx) / 2);
+
+    if (nums[midIdx] === target) return midIdx;
+
+    if (nums[midIdx] < target) {
+      leftIdx = midIdx + 1;
+    } else {
+      rightIdx = midIdx - 1;
+    }
+  }
+
+  return -1;
+}
+
+const numbers: number[] = [1, 3, 4, 5, 10, 11, 23, 50];
