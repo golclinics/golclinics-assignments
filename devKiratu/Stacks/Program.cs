c0eebfb0d8819e05cb4a000f891b51221f76b16f
devKiratu <dev.kiratu@gmail.com>
2021-11-04 08:36:08 +0300

    add solution for MinStack (Stacks & Queues Assignment 1)

--- devKiratu/Stacks/Program.cs
+++ devKiratu/Stacks/Program.cs
@@ -16,7 +16,17 @@ namespace Stacks
             Console.WriteLine($"is {b2} valid? : {StackExercises.IsValid(b2)}");
             Console.WriteLine($"is {b3} valid? : {StackExercises.IsValid(b3)}");
             Console.WriteLine($"is {b4} valid? : {StackExercises.IsValid(b4)}");
-        }
+
+      //MinStack - Stacks & Queues Assignment 1
+      var minStack = new MinStack();
+      minStack.Push(-2);
+      minStack.Push(0);
+      minStack.Push(-3);
+      var min1 = minStack.GetMin();
+      minStack.Pop();
+      var top1 = minStack.Top();
+      var min2 = minStack.GetMin();
+    }
 
 
     }
