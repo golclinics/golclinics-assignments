b87a0665f13a286f4fdf677b5724d6e106ec6f98
jacobricktified <gichira44@gmail.com>
2021-11-06 22:40:22 +0300

    revesed sentence using a temporary array

--- Jacob Gichira Mbuco/Arrays and Strings/reverse_sentence.java
+++ Jacob Gichira Mbuco/Arrays and Strings/reverse_sentence.java
@@ -1,17 +1,24 @@
-/*1. we will use the two pointer approach
-2. we will reverse all the characters in  a word
-3. we will reverse until the string end
-4. return the reversed array */
-import java.util.*;
-public class reverse_sentence(char []A){
-    int i=0; int j = A.length-1;
-    while (i<j){
-        char temp = A[i];
-        A[i]= A [j];
-        A[j]= temp;
-        i++; 
-        j--;
-    }
-     
 
+import java.util.*;
+//Write a function reverseSentence(A) that takes in an array of characters, A, and reverses the the "words" (not individual characters).
+/*Example:
+
+A = ['t','h','i','s',' ','i','s',' ','g','o','o','d']
+reverseSentence(A)
+A // ['g','o','o','d',' ','i','s',' ','t','h','i','s']
+*/
+public class reverse_sentence {
+    public void reverseSentence(char[] A) {
+        int i = 0;
+        int j = A.length - 1;
+        while (i < j) {
+            char temp = A[i];
+            A[i] = A[j];
+            A[j] = temp;
+            i++;
+            j--;
+        }
+    }
 }
+// time complexity: O(n)
+// space complexity: O(1)
