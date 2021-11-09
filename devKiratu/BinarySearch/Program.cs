c2613163fd460c9f9f20f0be58190ff94c80502d
devKiratu <dev.kiratu@gmail.com>
2021-11-09 15:21:57 +0300

    add solution to Iterative binary search

---
+++ devKiratu/BinarySearch/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace BinarySearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var search1 = new IterativeBinarySearch();
+            var nums = new int[]{1, 3, 4, 5, 10, 11, 23, 50};
+            var num = 11;
+            var result = search1.Exists(nums, num);
+            Console.WriteLine( $"{num} exists in [{string.Join(",", nums)}]? {result}");
+        }
+    }
+}
