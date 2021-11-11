9b238cefc3943c9e6ce7478c7addf463c27bdabb
James Njuguna <jameskinyua590@gmail.com>
2021-11-11 23:04:43 +0300

    Implement balancedBrackets function

--- JayKay24/Stacks and Queues 2/stacks_02.ts
+++ JayKay24/Stacks and Queues 2/stacks_02.ts
@@ -21,3 +21,37 @@ function reverseString(str: string): string {
 
   return reversedStr;
 }
+
+/**
+ * Returns "YES" if brackets is balanced, "NO" if unbalanced
+ * @param brackets - a sequence of brackets
+ * @param "YES" if brackets is balanced, "NO" if unbalanced
+ */
+function balancedBrackets(brackets: string): string {
+  const enum balancedState {
+    BALANCED = "YES",
+    UNBALANCED = "NO",
+  }
+
+  const stack = new Stack<string>("");
+
+  const brackedPairs: Map<string, string> = new Map([
+    ["(", ")"],
+    ["{", "}"],
+    ["[", "]"],
+  ]);
+
+  for (const char of brackets) {
+    if (brackedPairs.has(char)) {
+      stack.push(char);
+    } else {
+      if (brackedPairs.get(stack.peek()) !== char) {
+        return balancedState.UNBALANCED;
+      }
+
+      stack.pop();
+    }
+  }
+
+  return stack.empty ? balancedState.BALANCED : balancedState.UNBALANCED;
+}
