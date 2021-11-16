507790697480d1bdda557bfbcb261f22ec069e9d
James Njuguna <jameskinyua590@gmail.com>
2021-11-16 21:43:19 +0300

    Implement isSorted function

---
+++ JayKay24/Sorting 1/assignment-one.ts
@@ -0,0 +1,21 @@
+/**
+ * Determine if a collection is sorted
+ * @param integers - an array of integers
+ * @param ascending - order criteria, true for ascending, false for descending
+ *
+ * @returns Boolean indicating if the array is sorted
+ */
+function isSorted(integers: number[], ascending = true): boolean {
+  for (let i = 0; i < integers.length; i++) {
+    if (i > 0) {
+      if (ascending) {
+        if (integers[i] > integers[i + 1]) return false;
+      } else {
+        if (integers[i + 1] > integers[i]) return false;
+      }
+    }
+  }
+
+  return true;
+}
+// is an empty list, by definition, sorted?
