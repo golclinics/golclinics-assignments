888cafd11227d7f6f12a3ceb4701984ce103be11
devKiratu <dev.kiratu@gmail.com>
2021-11-17 23:03:09 +0300

    set up sorting project

---
+++ devKiratu/Sorting/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sorting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
