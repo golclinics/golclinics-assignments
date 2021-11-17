2bf5f41cf5c59aec2ae7183ff3ed154ede302872
James Njuguna <jameskinyua590@gmail.com>
2021-11-17 23:05:32 +0300

    Remove unnecessary conditional check

--- JayKay24/Sorting 1/assignment-1.ts
+++ JayKay24/Sorting 1/assignment-1.ts
@@ -2,18 +2,17 @@
  * Determine if a collection is sorted
  *
  * @param integers - an array of integers
- * @param ascending - order criteria, true for ascending, false for descending
+ * @param ascending - order criteria, true for ascending, false for descending.
+ * Default - ascending order
  *
  * @returns Boolean indicating if the array is sorted
  */
 function isSorted(integers: number[], ascending = true): boolean {
-  for (let i = 0; i < integers.length; i++) {
-    if (i > 0) {
-      if (ascending) {
-        if (integers[i] < integers[i - 1]) return false;
-      } else {
-        if (integers[i] > integers[i - 1]) return false;
-      }
+  for (let i = 1; i < integers.length; i++) {
+    if (ascending) {
+      if (integers[i] < integers[i - 1]) return false;
+    } else {
+      if (integers[i] > integers[i - 1]) return false;
     }
   }
 
