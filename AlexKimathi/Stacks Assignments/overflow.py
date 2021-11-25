6c8c7c05fafec1c2f721fca92253d3ef544860cd
Alex Kimathi <keemalekie@gmail.com>
2021-11-25 13:14:09 +0300

    implement the overflow of an array when pushing an element to a full stack

---
+++ AlexKimathi/Stacks Assignments/overflow.py
@@ -0,0 +1,35 @@
+ 
+from sys import maxsize
+
+def createStack():
+    stack = []
+    return stack
+ 
+
+def isEmpty(stack):
+    return len(stack) == 0
+ 
+
+def push(stack, item):
+    stack.append(item)
+    print(item + " pushed to stack ")
+     
+
+def pop(stack):
+    if (isEmpty(stack)):
+        return str(-maxsize -1) 
+     
+    return stack.pop()
+ 
+
+def peek(stack):
+    if (isEmpty(stack)):
+        return str(-maxsize -1) 
+    return stack[len(stack) - 1]
+ 
+  
+stack = createStack()
+push(stack, str(1))
+push(stack, str(2))
+push(stack, str(3))
+print(pop(stack) + " popped from stack")
