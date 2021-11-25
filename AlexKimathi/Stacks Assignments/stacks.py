6fb21975e4c1535a3f76811ad7ca71e9c3dca4cc
Alex Kimathi <keemalekie@gmail.com>
2021-11-25 12:37:40 +0300

    reversing a String using Stack Push and Pop operations

---
+++ AlexKimathi/Stacks Assignments/stacks.py
@@ -0,0 +1,34 @@
+def createStack():
+    stack=[]
+    return stack
+def size(stack):
+    return len(stack)
+def isEmpty(stack):
+    if size(stack) == 0:
+        return True
+def push(stack,item):
+    stack.append(item)
+
+def pop(stack):
+    if isEmpty(stack): return
+    return stack.pop()
+ 
+def reverse(string):
+    n = len(string)
+    
+    stack = createStack()
+
+    for i in range(0,n,1):
+        push(stack,string[i])
+
+    string=""
+
+    for i in range(0,n,1):
+        string+=pop(stack)
+         
+    return string
+    
+string="Gol clinics"
+string = reverse(string)
+print("Reversed string is " + string)
+ 
