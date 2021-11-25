67aba1eb042f8d9ed7878c1cfa28a3b0858a91cb
Alex Kimathi <keemalekie@gmail.com>
2021-11-25 12:59:21 +0300

    reversing a String using Stack Push and Pop operations

---
+++ AlexKimathi/Stacks Assignments/balanced.py
@@ -0,0 +1,46 @@
+def ISBalanced(expr):
+    stack = []
+ 
+    # Traversing the Expression
+    for char in expr:
+        if char in ["(", "{", "["]:
+ 
+            # Push the element in the stack
+            stack.append(char)
+        else:
+ 
+            # IF current character is not opening
+            # bracket, then it must be closing.
+            # So stack cannot be empty at this point.
+            if not stack:
+                return False
+            current_char = stack.pop()
+            if current_char == '(':
+                if char != ")":
+                    return False
+            if current_char == '{':
+                if char != "}":
+                    return False
+            if current_char == '[':
+                if char != "]":
+                    return False
+ 
+    # Check Empty Stack
+    if stack:
+        return False
+    return True
+ 
+ 
+# Driver Code
+if __name__ == "__main__":
+    expr = "{()}[]"
+ 
+    # Function call
+    if ISBalanced(expr):
+        print("YES")
+    else:
+        print("NO")
+
+      
+
+        
